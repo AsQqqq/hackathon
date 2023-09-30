b8923c2c5c04193e6332dfdc07371aed25afc2df
AsQ <danila.udodov.workamil@gmail.com>
2023-09-30 11:34:31 +0300

    Update v3

--- route.py
+++ route.py
@@ -1,6 +1,12 @@
 from flask import render_template, request, Flask, redirect
 from custom import info_, warning_
+import requests
+from config import url, headers
+
+
 app = Flask(__name__)
+
+
 
 @app.route('/', methods=['GET'])
 def index() -> render_template:
@@ -10,8 +16,21 @@ def index() -> render_template:
 @app.route('/send', methods=['POST', 'GET'])
 def send():
     """Получение данных из html"""
-    a = request.form['name']
-    info_(text=a)
+    a = request.form['message']
+
+    data = {
+        'phone': '89515009197',
+        'message': a,
+        'callback_url': 'http://localhost:3030/'
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    response_json = response.json()
+    message_id = response_json['id']
+
+    get_response = requests.get(url=f'{url}/{message_id}', headers=headers)
+    warning_(get_response.text)
+
     return redirect('http://localhost:3030')
 
 def upload_route() -> bool:
